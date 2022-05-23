dd0f1a83ab164a81cf208effa8c41076d3a78bb4
hernan_nicolas <hernanvarela45@gmail.com>
2022-05-23 14:02:48 -0300

    homework 1 resuelta

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,10 +3,34 @@
 function BinarioADecimal(num) {
   // tu codigo aca
 
+  num = num.toString();         // paso todo a string
+  let result = 0,
+  aux = num.length-1
+
+  for (let index = 0; index < num.length; index++) {
+    const element = num[index]
+    result += element*2**aux
+    aux--;
+  }
+
+  return result;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let result = []
+  if(typeof num != "number")
+   console.log("El dato ingresado no es un numero") 
+  else{
+    while(num >= 1){
+      result .push(num % 2)
+      num = Math.floor(num/2)
+    }
+    
+  }
+  result.reverse()
+  result = result.join("")
+  return result;
 
 }
 
